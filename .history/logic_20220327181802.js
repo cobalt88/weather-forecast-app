06001a75f0187c78e77db94a712b7c043da58350
cobalt88 <vincentteune@gmail.com>
2022-03-27 18:28:03 -0400

    3.27.22
    first commit. add in basic files, boilerplate HTML, and basic structure of javascript to fetch the data from the accuweather API

---
+++ .history/logic_20220327181802.js
@@ -0,0 +1,9 @@
+var requestUrl = 'http://dataservice.accuweather.com/locations/v1/topcities/50?apikey=%09WRy7rAgeG9pkGPZlac8sWxk9sXswNaMI';
+
+fetch(requestUrl)
+  .then(function(response){
+  return response.json();
+})
+.then(function(data){
+  console.log(data);
+})
