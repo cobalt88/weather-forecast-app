469313e160b0a932ea9e7ef2d847739c33515998
vincent teune <vincentteune@gmail.com>
2022-05-23 22:10:07 -0400

    5.23.22
    add gitignore and remove .history folder

--- .history/logic_20220327181802.js
+++
@@ -1,9 +0,0 @@
-var requestUrl = 'http://dataservice.accuweather.com/locations/v1/topcities/50?apikey=%09WRy7rAgeG9pkGPZlac8sWxk9sXswNaMI';
-
-fetch(requestUrl)
-  .then(function(response){
-  return response.json();
-})
-.then(function(data){
-  console.log(data);
-})
